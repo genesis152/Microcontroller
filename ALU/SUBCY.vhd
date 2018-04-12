3e163675b925761955767bc5e40d5f32ba98a477
claudiamates <38253984+claudiamates@users.noreply.github.com>
2018-04-12 22:30:43 +0300

    Update SUBCY.vhd

--- ALU/SUBCY.vhd
+++ ALU/SUBCY.vhd
@@ -1,23 +1,22 @@
 entity Scazator_complet is
-port(A,B,C_IN: in BIT_VECTOR(7 downto 0);
-DIF: out BIT_VECTOR(7 downto 0);
-CF,ZF: out BIT);
+port(A,B,C_IN: in std_logic_vector(7 downto 0);
+DIF: out std_logic_vector(7 downto 0);
+CF,ZF: out std_logic);
 end;
 
 architecture SUBCY of Scazator_complet is 
-signal C_OUT: BIT_VECTOR(7 downto 0);
+signal C_OUT: std_logic_vector(7 downto 0);
 begin
    process
 	variable J: INTEGER;
-	variable D: BIT_VECTOR(7 downto 0);
+	variable D: std_logic_vector(7 downto 0);
 	begin
 		for J in 0 to 7 loop
             D(J) := A(J) xor B(J) xor C_IN(J);
             C_OUT(J) <= (not A(J) and B(J)) or (not A(J) and C_IN(J)) or (B(J) and C_IN(J)); 
         end loop;
 	DIF <= D;
-	if (not(C_OUT = "00000000")) then CF <= '1';
-	end if;
+	CF <= C_OUT(7);
 	if(D = "00000000") then ZF <='1';
     end if;	
 	wait;
