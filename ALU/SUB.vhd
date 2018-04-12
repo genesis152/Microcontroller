4ea72473f52a32d39efb5c67e6cda38980e7a641
claudiamates <38253984+claudiamates@users.noreply.github.com>
2018-04-12 22:28:24 +0300

    Update SUB.vhd

--- ALU/SUB.vhd
+++ ALU/SUB.vhd
@@ -1,23 +1,22 @@
 entity Scazator is
-port(A,B: in BIT_VECTOR(7 downto 0);
-DIF: out BIT_VECTOR(7 downto 0);
-CF,ZF: out BIT);
+port(A,B: in std_logic_vector(7 downto 0);
+DIF: out std_logic_vectot(7 downto 0);
+CF,ZF: out std_logic);
 end;
 
 architecture SUB of Scazator is	 
-signal C_OUT: BIT_VECTOR(7 downto 0);
+signal C_OUT: std_logic_vector(7 downto 0);
 begin  
 	 process
 	variable J: INTEGER;
-	variable D: BIT_VECTOR(7 downto 0);
+	variable D: std_logic_vector(7 downto 0);
 	begin
 		for J in 0 to 7 loop
             D(J) := A(J) xor B(J);
             C_OUT(J) <= not A(J) and B(J); 
         end loop;
 	DIF <= D;
-	if (not(C_OUT = "00000000")) then CF <= '1';
-	end if;
+        CF<= C_OUT(7);
 	if(D = "00000000") then ZF <='1';
     end if;	
 	wait;
