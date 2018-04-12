de05bcbbbed827f8f3c51e1f6c80640a24b1a11d
genesis152 <38253549+genesis152@users.noreply.github.com>
2018-04-12 23:14:27 +0300

    Update AND.vhd

--- ALU/AND.vhd
+++ ALU/AND.vhd
@@ -1,11 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.ALL;
+
 entity Poarta_SI is
-port(A,B: in BIT_VECTOR(7 downto 0);
-Y: out BIT_VECTOR(7 downto 0);
-CF,ZF: out BIT);
+port(A,B: in STD_LOGIC_VECTOR(7 downto 0);
+Y: out STD_LOGIC_VECTOR(7 downto 0);
+CF,ZF: out STD_LOGIC);
 end;  
 
 architecture SI of Poarta_SI is
+signal K: STD_LOGIC_VECTOR(7 downto 0);
 begin
-Y <= A and B; 
+K <= A and B;
+Y <= K;
 CF <= '0';
+ZF <= not(K(0) or K(1) or K(2) or K(3) or K(4) or K(5) or K(6) or K(7));
 end; 
