a7a98161f2de59764653c36edcaaeb985750b4fd
genesis152 <38253549+genesis152@users.noreply.github.com>
2018-04-12 21:11:43 +0300

    Update my_package.vhd

--- my_package.vhd
+++ my_package.vhd
@@ -2,7 +2,8 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.numeric_std.ALL;
 
-package my_package is
-	type M256x16 is array(7 downto 0) of std_logic_vector(15 downto 0);
-	type M16x8 is array(15 downto 0) of bit_vector(7 downto 0);
-end my_package;
+package tip is
+	type VEC is array(15 downto 0) of STD_LOGIC_VECTOR(7 downto 0);
+	type M256x16 is array(7 downto 0) of STD_LOGIC_vector(15 downto 0);
+	type M16x8 is array(15 downto 0) of STD_LOGIC_vector(7 downto 0);
+end tip;
