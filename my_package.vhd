7dd22ede43f26fde80f65971b290aa6ee78c0b48
genesis152 <38253549+genesis152@users.noreply.github.com>
2018-05-02 18:44:56 +0300

    Update my_package.vhd

--- my_package.vhd
+++ my_package.vhd
@@ -5,5 +5,6 @@ use ieee.numeric_std.ALL;
 package tip is
 	type VEC is array(15 downto 0) of STD_LOGIC_VECTOR(7 downto 0);
 	type M256x16 is array(7 downto 0) of STD_LOGIC_vector(15 downto 0);
-	type M16x8 is array(15 downto 0) of STD_LOGIC_vector(7 downto 0);
+	type M168 is array(15 downto 0) of STD_LOGIC_vector(7 downto 0);
+	type M816 is array(7 downto 0) of STD_LOGIC_VECTOR(15 downto 0);
 end tip;
