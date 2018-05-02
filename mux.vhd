98743e59fdb907562bb279b612a47e1e520003af
genesis152 <38253549+genesis152@users.noreply.github.com>
2018-05-02 18:43:28 +0300

    Update mux.vhd

--- mux.vhd
+++ mux.vhd
@@ -1,23 +1,22 @@
+--MERGE FARA BUGURI
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.numeric_std.ALL;
 use tip.all;
 
 entity Mux16la1 is
- port(I: in VEC;
- 	  EN: in BIT;
-      SEL: in STD_LOGIC_VECTOR(3 downto 0);
-      OUT_A: out STD_LOGIC_VECTOR(7 downto 0) );
+ port(MUX_IN: in M168;
+ 	      EN: in BIT;
+         SEL: in STD_LOGIC_VECTOR(3 downto 0);
+     MUX_OUT: out STD_LOGIC_VECTOR(7 downto 0) );
 end  Mux16la1;
 
 architecture Mux1 of Mux16la1 is
 begin
- ENABLE: process(SEL,EN)
- begin
- 	if EN='1' then
- 		OUT_A<=I(to_integer(unsigned(SEL)));
-	else
-		OUT_A<="00000000";
-	end if;
-end process;
+BLOCK_MUX: block
+begin
+	MUX_OUT<=MUX_IN(to_integer(unsigned(SEL))) when (EN='1')
+	else "ZZZZZZZZ";
+end block BLOCK_MUX;
 end Mux1;
