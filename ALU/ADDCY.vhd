5b34eeb94a3c9f527be01e601d417b003c5d7ee3
claudiamates <38253984+claudiamates@users.noreply.github.com>
2018-05-03 17:27:27 +0300

    Update ADDCY.vhd

--- ALU/ADDCY.vhd
+++ ALU/ADDCY.vhd
@@ -1,25 +1,33 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
 entity Sumator_complet is
-port(A,B,C_IN: in std_logic_vector(7 downto 0);
-Y: out std_logic_vector(7 downto 0);
+port(A,B: in std_logic_VECTOR(7 downto 0);
+C_IN: in	 std_logic;
+Y: out std_logic_VECTOR(7 downto 0);
 CF,ZF: out std_logic);
 end;
 
 architecture ADDCY of Sumator_complet is
- signal I,C_OUT: std_logic_vector(7 downto 0);
 begin 	
-	process
-	variable J: INTEGER;
-	variable S: std_logic_vector(7 downto 0);
+	process(A,B)
+	variable S: std_logic_VECTOR(7 downto 0); 
+	
+	variable C_I: std_logic;
 	begin
-		for J in 0 to 7 loop
-			I(J) <= A(J) xor B(J);
-			S(J) := I(J) xor C_IN(J);
-            C_OUT(J) <= (A(J) and B(J)) or (I(J) and C_IN(J));
+	
+		S(0) := (A(0) xor B(0)) xor C_IN;	-- se calc suma pe ultimul bit cu carry in
+		for J in 1 to 7 loop
+			
+		    C_I := A(J-1) and B(J-1);	 --se foloseste C_I pt a retine carry-ul pt fiecare bit
+			S(J) := (A(J) xor B(J)) xor C_I; --se aduna carry de la bitul anterior
+           
         end loop;
-  Y <= S;
-CF <= C_OUT(7);
-if(S = "00000000") then ZF <='1';
+  Y <= S; 
+  
+CF <= C_I;
+if(S = "00000000") then ZF <='1'; 
+	else ZF <= '0';
 end if;
-wait;
 end process;
 end;
