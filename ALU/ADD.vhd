be974cc95ec85dc1e2a21a8b61943a14a36ce58b
claudiamates <38253984+claudiamates@users.noreply.github.com>
2018-05-03 17:24:34 +0300

    Update ADD.vhd

--- ALU/ADD.vhd
+++ ALU/ADD.vhd
@@ -1,3 +1,6 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
 entity Semi_sumator is
 port(A,B: in std_logic_vector(7 downto 0);
 SUM: out std_logic_vector(7 downto 0);
@@ -5,24 +8,25 @@ CF,ZF:out std_logic);
 end;
 
 architecture ADD of Semi_sumator is	  
-signal C_OUT: std_logic_vector(7 downto 0);
 begin	
-		process
-	variable I: INTEGER;
-	variable S: std_logic_vector(7 downto 0);
-	begin
-		for I in 0 to 7 loop
-			S(I) := A(I) xor B(I);
-			C_OUT(I) <= A(I) and B(I);
+		process	(A,B)
+	variable S: std_logic_vector(7 downto 0);  
+	variable C :std_logic;
+	begin 
+		C := A(0) and B(0);	 	-- pe bitul cel mai nesemnificativ nu intra carry	
+		S(0) :=	A(0) xor B(0);
+		for I in 1 to 7 loop
+			S(I) := (A(I) xor B(I)) xor C;     -- C e un carry interior pt toti biti inafara de cel mai nesemnificativ
+			C := A(I) and B(I);
+	
 		end loop;	
  SUM <= S;
-CF <= C_OUT(7);
-if (S="00000000") then ZF <= '1';	
+CF <= C; -- Carry Flagul ia ultima valoare a lui C
+if (S="00000000") then ZF <= '1';
+	else ZF <= '0';
 	end if;
-wait;
+
 end process;
-end ADD;	
-
-
+end ADD;
 
 
